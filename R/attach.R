9d23f475da01e1a6157797b6990cc32e6e85477d
Fin <Fin.Yang@monash.edu>
2019-04-15 11:45:13 +1000

    drop attach

--- R/attach.R
+++ R/attach.R
@@ -1,8 +1,8 @@
-.onAttach <- function(...)
-{
-  library(help=lazytex)$info[[1]] -> version
-  version <- version[pmatch("Version",version)]
-  um <- strsplit(version," ")[[1]]
-  version <- um[nchar(um)>0][2]
-  packageStartupMessage(paste("This is demography",version,"\n"))
-}
+# .onAttach <- function(...)
+# {
+#   library(help=lazytex)$info[[1]] -> version
+#   version <- version[pmatch("Version",version)]
+#   um <- strsplit(version," ")[[1]]
+#   version <- um[nchar(um)>0][2]
+#   packageStartupMessage(paste("This is demography",version,"\n"))
+# }
