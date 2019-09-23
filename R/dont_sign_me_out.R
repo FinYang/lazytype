9db2172f6936efd91b623d5d937a64f5e1a8a7ab
Fin <Fin.Yang@monash.edu>
2019-09-23 14:15:40 +1000

    add day display

--- R/dont_sign_me_out.R
+++ R/dont_sign_me_out.R
@@ -1,6 +1,7 @@
 #' @export
 dont_sign_me_out <- function(hours = 3.5){
   hour <- 0
+  day <- 0
   for(i in 1:(12*hours)){
     Sys.sleep(300)
     # if(i %% 2 == 0) rMouse::move(800,0) else rMouse::move(700,0)
@@ -20,11 +21,15 @@ dont_sign_me_out <- function(hours = 3.5){
     rMouse::move(x,y, failSafe = FALSE)
     if(((i*5) %% 60) ==0)
       hour <- hour + 1
-    if(hour != 0)
-      cat("\r",paste0(hour, " hour", ifelse(hour ==1, " ", "s ") ,i*5-60*hour, " mins"))
-    else
-      cat("\r",paste0(i*5, " mins"))
-
+    if(((i*5) %% (60*24)) ==0)
+      day <- day + 1
+    if(day != 0){
+      cat("\r",paste0(day, " day",ifelse(day ==1, " ", "s "), hour-24*day, " hour", ifelse(hour ==1, " ", "s ") , ifelse(i*5-60*hour >5, "", " "), i*5-60*hour, " mins"))
+    } else if(hour != 0){
+      cat("\r",paste0(hour, " hour", ifelse(hour ==1, " ", "s ") , ifelse(i*5-60*hour >5, "", " "),i*5-60*hour, " mins"))
+    } else {
+      cat("\r",paste0( ifelse(i >1, "", " "), i*5, " mins"))
+    }
     flush.console()
   }
 }
