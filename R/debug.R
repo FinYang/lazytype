5aa45f87463887d5549dd65a1a92f1fb87e409d8
Fin Yang <yangyangzhuoran@gmail.com>
2022-08-03 15:41:43 +1000

    Add addin to store and rerun expression to debug

---
+++ R/debug.R
@@ -0,0 +1,29 @@
+debug_env <- new.env(parent = emptyenv())
+
+get_select_text <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  if('#console' %in% context$id){
+    message("Cursor at console")
+    text <- context$contents
+  } else {
+    text <- context$selection[[1]]$text
+  }
+  text
+}
+
+#' @export
+store_debug_expr <- function(expr = NULL){
+  expr <- rlang::enexpr(expr)
+  if(is.null(expr)){
+    debug_env[["debug_expr"]] <- get_select_text()
+  } else {
+    debug_env[["debug_expr"]] <- deparse(expr)
+  }
+  message("Expression stored")
+  invisible(NULL)
+}
+
+#' @export
+rerun_debug <- function(){
+  eval(parse(text = debug_env[["debug_expr"]]))
+}
